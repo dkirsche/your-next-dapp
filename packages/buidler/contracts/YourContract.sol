cdce112b4292c7623620e159cf19df1b8443c89a
Daniel Kirsche <dkirsche@Daniels-MacBook-Pro.local>
2020-09-21 19:59:00 -0500

    added counter to contract

--- packages/buidler/contracts/YourContract.sol
+++ packages/buidler/contracts/YourContract.sol
@@ -7,7 +7,10 @@ contract YourContract {
   event SetPurpose(address sender, string purpose);
 
   string public purpose = "🛠 Programming Unstoppable Money";
-
+  uint8 public count = 1;
+  function dec() public {
+    count = count - 1;
+  }
   function setPurpose(string memory newPurpose) public {
     purpose = newPurpose;
     console.log(msg.sender,"set purpose to",purpose);
